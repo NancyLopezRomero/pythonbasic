09f065f3142e9442bd9f2bfb63f2c10179d94f52
NancyLopezRomero <99849137+NancyLopezRomero@users.noreply.github.com>
2022-02-17 13:52:57 -0600

    Tarea_OperacionesMatemáticas(feb16)

---
+++ Ejercicios Python/Operacionesmatematicas.py
@@ -0,0 +1,52 @@
+# ENTRADAS DE DATOS
+# Obtener los 2 números
+
+número1 = 10
+número2 = 2
+
+
+
+
+
+# PROCESOS (Operaciones, cálculos matemáticos y Lógicos)
+# Operaciones de los dos números
+
+suma = número1 + número2
+resta = número1 - número2
+multiplicación = número1 * número2
+división = número1 / número2
+potencia1 = número1 ** número2
+potencia2= pow(número1, número2)
+cuadrado = pow(número1,2) #número1**2
+cubo = pow(número2,3) #número1**3
+
+
+
+
+# SALIDAS DE DATOS
+# Resultado de las operaciones
+
+print("La suma =", suma)
+print("La suma = "+str(suma))# concatenar
+print(f"La suma = {suma}") # interpolación
+print("La resta = ", resta)
+print("La resta = "+str(resta)) # concatenar
+print(f"La resta = {resta}") # interpolación
+print("La multiplicación = ", multiplicación)
+print("La multiplicacion = " +str(multiplicación)) # concatenar
+print(f"La multiplicación = {multiplicación}") # interpolación
+print("La división =", división)
+print("La división =" + str(división))
+print(f"La división ={división}")
+print("La potencia1 = ", potencia1)
+print("La potencia1 = " + str(potencia1))
+print(f"La potencia1 = {potencia1}")
+print("La potencia 2 = ", potencia2)
+print("La potencia2 = "+ str(potencia2))
+print(f"La potencia2 = {potencia2}")
+print("El cuadrado = ", cuadrado)
+print("El cuadrado = " + str (cuadrado))
+print(f"El cuadrado = {cuadrado}")
+print("El cubo = ", cubo)
+print("El cubo = " + str(cubo))
+print(f"El cubo = {cubo}")
